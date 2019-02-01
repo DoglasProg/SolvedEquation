3db2f7e6638357fa8cd7e5a8014b0f1c374b9480
DoglasProg <dogllas12@hotmail.com>
2019-02-01 00:04:20 -0300

    Descrição das operações

--- src/com/soverequation/main/main.java
+++ src/com/soverequation/main/main.java
@@ -8,23 +8,24 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		/*
+		//operações com parametros e valores
+		
+		Expression exp2 = new Expression("(50)-x-x-x");
+		exp2.setVariable("x", 10L);
+		System.out.println(exp2.resolve());
+		
+		//Operações com operadores especiais 
+		
 		Expression exp = new Expression("y+z*((10+10)+cos(10)-sin(x)*tan(8))");
 		exp.setVariable("x", 8L);
-		exp.setVariable("y", 8L);
-		exp.setVariable("z", 8L);
-		System.out.println(exp.resolve());*/
+		exp.setVariable("y", 10L);
+		exp.setVariable("z", 20L);
+		System.out.println(exp.resolve());
 		
-		/*Expression exp2 = new Expression("(50)-x-x-x");
-		exp2.setVariable("x", 10L);
-		System.out.println(exp2.resolve());*/
-		
-		
+		//Operações com data
 		Expression exp3 = new Expression("a+15");
 		LocalDate local = LocalDate.now();
-		LocalDate loc = LocalDate.of(2019, 2, 10);
 		exp3.setVariableLocalDate("a", local);
-		
 		System.out.println(exp3.resolveLocaldate());
 		
 
